8fc51fc01f08dc7f108856205f69dde0a3647194
MinTzuLo <m5lo@ucsd.edu>
2014-10-21 13:47:44 -0700

    run fit and save parameters in the disk

--- analyze_mosquito_data_script.py
+++ analyze_mosquito_data_script.py
@@ -2,5 +2,8 @@ import pandas as pd
 import analyze_mosquito_data_lib as mosquito_lib
 filename = "A1_mosquito_data.csv"
 data = pd.read_csv(filename)
-print data.head()
-mosquito_lib.analyze(data)
+data["temperature"]=mosquito_lib.fahr_to_celsius(data["temperature"])
+parameters = mosquito_lib.analyze(data)
+print parameters
+parameters.to_csv("parameters.csv")
+
