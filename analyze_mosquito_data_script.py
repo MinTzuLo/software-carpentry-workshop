2c68add118fb0ee9cbac3e4be4b897e17d9b6dd6
MinTzuLo <m5lo@ucsd.edu>
2014-10-21 14:08:59 -0700

    analyze

--- analyze_mosquito_data_script.py
+++ analyze_mosquito_data_script.py
@@ -3,7 +3,7 @@ import analyze_mosquito_data_lib as mosquito_lib
 filename = "A1_mosquito_data.csv"
 data = pd.read_csv(filename)
 data["temperature"]=mosquito_lib.fahr_to_celsius(data["temperature"])
-parameters = mosquito_lib.analyze(data)
+parameters = mosquito_lib.analyze(data, "plot.png")
 print parameters
 parameters.to_csv("parameters.csv")
 
